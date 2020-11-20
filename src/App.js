72cf4f759642242adf84018fc12d3dd01665c621
Harsh Nabera <harshatkabralogitech@gmail.com>
2020-11-20 21:58:05 +0530

    mobile view created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Collection from "./Components/Cards/Collection";
+import Info from "./Components/Cards/Info";
+import LikedProduct from "./Components/Cards/LikedProduct";
+import Pair from "./Components/Cards/Pair";
+import Shop from "./Components/Cards/Shop";
 import Product from "./Components/Product/Product";
 import FullWidthTabs from "./Components/Product/ProductTab";
 
@@ -15,6 +20,13 @@ class App extends Component {
         <Router>
           <Route path="/" render={() => <Product />} />
           <Route path="/" render={() => <FullWidthTabs />} />
+          <Route path="/" render={() => <Pair />} />
+          <Route path="/" render={() => <Info />} />
+          <Route path="/" render={() => <LikedProduct />} />
+          <Route path="/" render={() => <Collection />} />
+          <Route path="/" render={() => <Shop />} />
+          <br />
+          <br />
         </Router>
       </Fragment>
     );
---
+++ src/Components/Cards/Collection.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from "react";
+import { Container, Grid, Paper, Button } from "@material-ui/core";
+import Data from "./PairData";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import "./cards.css";
+
+class Collection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <br />
+        <Container maxWidth="md">
+          <br />
+          <h3 style={{ textAlign: "center" }} className="heading">
+            DISCOVER THE COLLECTION{" "}
+          </h3>
+          <br />
+          <Grid container spacing={16} style={{ textAlign: "center" }}>
+            {Data.length > 0 &&
+              Data.map((e, i) => (
+                <Grid
+                  xl={3}
+                  lg={3}
+                  md={3}
+                  sm={12}
+                  xs={12}
+                  key={i}
+                  style={{ textAlign: "center" }}
+                >
+                  <Paper elevation={0}>
+                    <img src={e.img} height="200"></img>
+                  </Paper>
+                  <div className="text">{e.name}</div>
+                  <p className="text">Rs. {e.price}</p>
+                </Grid>
+              ))}
+          </Grid>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+export default Collection;
